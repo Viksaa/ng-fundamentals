9ef4394bdd52cc3ff23e7da1ea4a08c91c81d644
Viktor Stojkoski <viktor.98@hotmail.com>
2019-07-08 20:31:14 +0200

    Added Filtering and Sorting to the Session List

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared/index';
 
 @Component({
@@ -6,11 +6,40 @@ import { ISession } from '../shared/index';
     templateUrl: './session-list.component.html'
 })
 
-export class SessionListComponent implements OnInit {
+export class SessionListComponent implements OnInit, OnChanges {
 
     @Input() sessions: ISession[];
+    @Input() filterBy: string;
+    @Input() sortBy: string;
+    visibleSessions: ISession[] = [];
 
     constructor() { }
 
     ngOnInit() { }
+
+    ngOnChanges() {
+        if (this.sessions) {
+            this.filterSessions(this.filterBy);
+            this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc);
+        }
+    }
+
+    filterSessions(filter: string) {
+        if (filter === 'all') {
+            this.visibleSessions = this.sessions.slice(0);
+        } else {
+            this.visibleSessions = this.sessions.filter(s => {
+                return s.level.toLocaleLowerCase() === filter;
+            })
+        }
+    }
+}
+function sortByNameAsc(s1: ISession, s2: ISession) {
+    if (s1.name > s2.name) return 1;
+    else if (s1.name === s2.name) return 0;
+    else return -1;
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+    return s2.voters.length - s1.voters.length;
 }
