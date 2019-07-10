37a126e4feb2b76567c003fe2d61e5516439023d
Viktor Stojkoski <viktor.98@hotmail.com>
2019-07-10 18:30:57 +0200

    changed the Event and Voter Service to Handle HTTP Calls

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -2,7 +2,7 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { Subject, Observable, of } from 'rxjs';
 import { IEvent } from './index';
 import { ISession } from './event.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 
 @Injectable({
@@ -17,49 +17,27 @@ export class EventService {
             .pipe(catchError(this.handleError<IEvent[]>('getEvents', [])));
     }
 
+    getEvent(id: number): Observable<IEvent> {
+        return this.http.get<IEvent>('/api/events/' + id)
+            .pipe(catchError(this.handleError<IEvent>('getEvent')));
+    }
+
+    saveEvent(event) {
+        let options = { headers: new HttpHeaders({ "Content-Type": "application/json" }) };
+        return this.http.post<IEvent>('/api/events', event, options)
+            .pipe(catchError(this.handleError<IEvent[]>('saveEvent')));
+    }
+
+    searchSessions(searchTerm: string): Observable<ISession[]> {
+        return this.http.get<ISession[]>('/api/sessions/search?search=' + searchTerm)
+            .pipe(catchError(this.handleError<ISession[]>('searchSessions')));
+    }
+
     private handleError<T>(operation = 'operation', result?: T) {
         return (error: any): Observable<T> => {
             console.error(error);
             return of(result as T);
         }
-    }
-
-    getEvent(id: number): IEvent {
-        return EVENTS.find(event => event.id === id);
-    }
-
-    saveEvent(event) {
-        event.id = 999;
-        event.session = [];
-        EVENTS.push(event);
-    }
-
-    updateEvent(event) {
-        let index = EVENTS.findIndex(i => i.id === event.id);
-        EVENTS[index] = event;
-    }
-
-    searchSessions(searchTerm: string) {
-        let term = searchTerm.toLocaleLowerCase();
-        let results: ISession[] = [];
-
-        EVENTS.forEach(event => {
-            let matchingSessions = event.sessions.filter(session =>
-                session.name.toLocaleLowerCase().includes(term)
-            );
-            matchingSessions = matchingSessions.map((session: any) => {
-                session.eventId = event.id;
-                return session;
-            });
-
-            results = results.concat(matchingSessions);
-        });
-
-        let emitter = new EventEmitter(true);
-
-        setTimeout(() => emitter.emit(results), 100);
-
-        return emitter;
     }
 }
 
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,9 +2,9 @@ import {
     EventsListComponent,
     EventDetailsComponent,
     CreateEventComponent,
-    EventRouteActivator,
     EventsListResolver,
-    CreateSessionComponent
+    CreateSessionComponent,
+    EventResolver
 } from './events/index';
 
 import { Routes } from '@angular/router'
@@ -13,7 +13,7 @@ import { Error404Component } from './errors/404.component';
 export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events', component: EventsListComponent, resolve: { events: EventsListResolver } },
-    { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+    { path: 'events/:id', component: EventDetailsComponent, resolve: { event: EventResolver } },
     { path: 'events/session/new', component: CreateSessionComponent },
     { path: '404', component: Error404Component },
     { path: '', pathMatch: 'full', redirectTo: '/events' },
